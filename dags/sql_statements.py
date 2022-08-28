4fc5ca74d5f045dc65c4fce44d99934d793d29ea
rmbborges <borges.ricardomb@gmail.com>
2022-08-28 09:15:57 -0300

    creation queries

--- dags/sql_statements.py
+++ dags/sql_statements.py
@@ -35,65 +35,125 @@ CREATE_RAW__TAXIGOV_CORRIDAS_TABLE = """
 CREATE_DIM_REQUESTS_TABLE = """
     DROP TABLE IF EXISTS dim_requests;
 
-    CREATE TABLE IF NOT EXISTS dim_requests (
-        id BIGINT IDENTITY NOT NULL,
-        requested_by VARCHAR(500),
-        requested_at TIMESTAMP,
-        approved_at TIMESTAMP,
-        reason VARCHAR(200),
-        status VARCHAR(200),
-        requested_dropoff_latitude DECIMAL(16,2),
-        requested_dropoff_longitude	DECIMAL(16,2),
-        commentary VARCHAR(2000)
-    );
+    CREATE TABLE IF NOT EXISTS dim_requests AS 
+        SELECT
+            TO_HEX(
+                SHA2(
+                    CONCAT(
+                        CAST(data_abertura AS STRING), 
+                        nome_orgao
+                    ),
+                    256
+                )
+            ) AS id,
+            UPPER(nome_orgao) AS requested_by,
+            data_abertura AS requested_at,
+            data_despacho AS approved_at,
+            UPPER(motivo_corrida) AS reason,
+            destino_solicitado_latitude AS requested_dropoff_latitude,
+            destino_solicitado_longitude AS requested_dropoff_longitude,
+            conteste_info AS commentary
+        FROM
+            raw__taxigov_corridas;
 """
 
 CREATE_DIM_RIDES_TABLE = """
     DROP TABLE IF EXISTS dim_rides;
 
-    CREATE TABLE IF NOT EXISTS dim_rides (
-        id BIGINT IDENTITY NOT NULL,
-        request_id INT,
-        started_at TIMESTAMP,
-        ended_at TIMESTAMP,
-        pickup_latitude DECIMAL(16,2),
-        pickup_longitude DECIMAL(16,2),
-        dropoff_latitude DECIMAL(16,2),
-        dropoff_longitude DECIMAL(16,2),
-        distance DECIMAL(16,2),
-        cost DECIMAL(16,2) 
-    );
+    CREATE TABLE IF NOT EXISTS dim_rides AS 
+        SELECT
+            TO_HEX(
+                SHA2(
+                    qru_corrida,
+                    256
+                )
+            ) AS id,
+            TO_HEX(
+                SHA2(
+                    CONCAT(
+                        CAST(data_abertura AS STRING), 
+                        nome_orgao
+                    ),
+                    256
+                )
+            ) AS request_id
+            data_inicio AS started_at,
+            data_final AS ended_at,
+            origem_latitude AS pickup_latitude,
+            origem_longitude AS pickup_longitude,
+            destino_efetivo_latitude AS dropoff_latitude,
+            destino_efetivo_longitude AS dropoff_longitude,
+            km_total AS distance,
+            valor_corrida AS cost
+        FROM
+            raw__taxigov_corridas;
 """
 
 CREATE_DIM_DATES_TABLE = """
     DROP TABLE IF EXISTS dim_dates;
 
-    CREATE TABLE IF NOT EXISTS dim_dates (
-        id BIGINT IDENTITY NOT NULL,
-        ts TIMESTAMP,
-        date DATE,
-        day INTEGER,
-        month INTEGER,
-        year INTEGER,
-        day_of_week INTEGER,
-        is_weekday BOOLEAN
-    );
+    CREATE TABLE IF NOT EXISTS dim_dates AS 
+        WITH
+        distinct_timestamps AS (
+            SELECT
+                ARRAY(
+                    data_abertura,
+                    data_despacho,
+                    data_local_embarque,
+                    data_inicio,
+                    data_final
+                ) AS timestamp_array
+            FROM
+                raw__taxigov_corridas
+        )
+        
+        SELECT
+            DISTINCT
+                ts,
+                DATE(ts) AS date,
+                EXTRACT(MONTH FROM ts) AS month,
+                EXTRACT(YEAR FROM ts) AS year,
+                EXTRACT(DOW FROM ts) AS day_of_week,
+                CASE
+                    WHEN EXTRACT(DOW FROM ts) IN (1, 7) THEN TRUE
+                    ELSE FALSE 
+                END AS is_weekend
+        FROM
+            distinct_timestamps,
+            distinct_timestamps.timestamp_array AS ts;
 """
 
 CREATE_FACT_DAILY_RIDES_TABLE = """
     DROP TABLE IF EXISTS fact_daily_rides;
 
-    CREATE TABLE IF NOT EXISTS fact_daily_rides (
-        id BIGINT IDENTITY NOT NULL,
-        date DATE,
-        ride_requests_count INTEGER,
-        approved_ride_requests_count INTEGER,
-        total_rides_cost DECIMAL(16, 2),
-        total_cost_per_kilometer DECIMAL(16, 2),
-        average_time_to_approve_request DECIMAL(16, 2),
-        average_ride_duration_time DECIMAL(16, 2),
-        average_ride_cost DECIMAL(16, 2),
-        average_ride_distance DECIMAL(16, 2)
-    );
+    CREATE TABLE IF NOT EXISTS fact_daily_rides AS
+        SELECT
+            DATE(started_at) AS date,
+            COUNT(DISTINCT(ride_requests.id)) AS ride_requests_count,
+            COUNT(DISTINCT(rides.id)) AS rides_count
+            SUM(cost) AS total_rides_cost,
+            SUM(cost)/COUNT(DISTINCT(rides_count)) AS average_cost_per_kilometer,
+            AVG(
+                DATEDIFF(
+                    MINUTE,
+                    started_at,
+                    ended_at
+                )
+            ) AS average_ride_duration,
+            AVG(
+                DATEDIFF(
+                    MINUTE,
+                    requested_at,
+                    approved_at
+                )
+            ) AS average_ride_request_sla,
+            AVG(cost) AS average_ride_cost,
+            AVG(distance) AS average_ride_distance
+        FROM
+            dim_rides
+        LEFT JOIN 
+            dim_requests ON dim_rides.request_id = dim_requests.id
+        GROUP BY 
+            date;
 """
 
